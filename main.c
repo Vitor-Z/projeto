ddffe57b380b8a168c6c8df3be7f42db4a6e03ee
Vitor-Z <vitorzavaglia2@gmail.com>
2025-02-04 10:42:02 -0300

    Inicio projeto

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/4/2025 10:25:47 AM
+ *  Author: Vitor
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
