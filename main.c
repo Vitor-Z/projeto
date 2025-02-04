236a274866d07ef3c2f02fc2b41d635398d35393
Vitor-Z <vitorzavaglia2@gmail.com>
2025-02-04 10:52:55 -0300

    Base do projeto - inicio

--- main.c
+++ main.c
@@ -6,11 +6,17 @@
  */ 
 
 #include <xc.h>
-
+#define F_CPU 16000000
+#include <util/delay.h>
 int main(void)
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	DDRB = (1<<5); //configura PB5 como saída
+	PORTB &= ~(1<<5); //desliga PB5
+	while(1)
+	{
+		PORTB |= 0b00100000; // aciona PB5
+		_delay_ms(500);
+		PORTB &= 0b11011111; // desaciona PB5
+		_delay_ms(500);
+	}
 }
